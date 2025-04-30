8a976dd310246c7c623110b3e76d9aaf7b4ae4ec
Shourya4641 <shouryasarkar001@gmail.com>
2025-04-30 22:57:41 +0530

    feat: theme toggle

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
+
+export default function Home() {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle />
+    </div>
+  );
+}
